920b71121eef6385e0bce8c1b86996bf899d74b0
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-11-11 15:08:44 -0300

    Primeiro FT especificado nos comentarios, utilizando agora o unittest.

--- funcional_test.py
+++ funcional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith escucho hablar de una nueva aplicación online interesante para
+        # lista de tareas. Ella decide verificar la pagina de inicio
+        self.browser.get('http://localhost:8000')
+
+        # Ella percibe que el título de la pagina y el encabezado mencionan listas de
+        # tareas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
