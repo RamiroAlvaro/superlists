c1b2251798aac2cdfe31106835c2c2f212dce0ac
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-11-12 15:18:55 -0300

    Refactor FT

--- funcional_test.py
+++ funcional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith escucho hablar de una nueva aplicación online interesante para
         # lista de tareas. Ella decide verificar la pagina de inicio
@@ -37,14 +42,22 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" como un iten de la lista de tareas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        # Todavia hay una caja de texto para acresentar otro iten. Ella inserta
+        # 'Use peacock feathers to make a fly'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        # La pagina es actualizada nuevamente y ahora muestra dos itens en su lista
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith se pregunta si el sitio recordara de su lista. Entonces nota
+        # que el sitio genero una URL unica para ella -- hay un pequeño
+        # texto explicativo para eso.
         self.fail('Finish the test!')
 
 
